3712fb760961c73657a633a799d31e47a16188d2
SangMin-Code <ragdexplorer@gmail.com>
2021-04-02 12:47:53 +0900

    2021.4.2_문제풀이/복습2

--- Programmers/level3/disk_controller.py
+++ Programmers/level3/disk_controller.py
@@ -23,6 +23,23 @@ def my(jobs:List[int])->int:
             now+=1
     return answer//len(jobs)
 
+def practice(jobs:List[int])->int:
+    answer, now, i = 0,0,0
+    start = -1
+    heap =[]
+    while i < len(jobs):
+        for j in jobs:
+            if start < j[0] <=now:
+                heapq.heappush(heap,[j[1],j[0]])
+        if heap:
+            current = heapq.heappop(heap)
+            start = now
+            now += current[0]
+            answer+=(now-current[1])
+            i+=1
+        else:
+            now+=1
+    return answer//len(jobs)
 
 
 
