6a4f5ba30b73a51f25ae6f582ab7e2ac852e9edb
SangMin-Code <ragdexplorer@gmail.com>
2021-04-03 18:15:03 +0900

    2021.4.3 복습

--- Programmers/level3/2_by_N_tiling.py
+++ Programmers/level3/2_by_N_tiling.py
@@ -14,6 +14,17 @@ def my(n:int)->int:
     print(l)
     return l[n]
 
+def practice(n:int)->int:
+    l = [0] * (n + 1)
+    l[1], l[2] = 1, 2
+    for i in range(3, n + 1):
+        if i % 2 == 0:
+            l[i] = 2 * l[i - 2] + l[i - 3]
+        else:
+            l[i] = l[i - 1] + l[i - 2]
+    print(l)
+    return l[n]
+
 def test(n:int)->int:
     p,pp,ppp,val = 2,1,0,0
     for i in range(3,n+1):
