1f9166bca84e2da39d3bef9a5b56f4a5cfd1e175
SangMin-Code <ragdexplorer@gmail.com>
2021-04-30 20:46:44 +0900

    2021.4.30 문제풀이_global제거

--- Programmers/level3/make_all_zeros.py
+++ Programmers/level3/make_all_zeros.py
@@ -3,7 +3,6 @@ import sys
 from typing import List
 import collections
 sys.stdin = open('input/make_all_zeros')
-
 '''
 def my(a:List[int], edges:[List[List[int]]])->int:
     answer = 0
@@ -13,24 +12,27 @@ def my(a:List[int], edges:[List[List[int]]])->int:
     for n1,n2 in edges:
         dic[n1].append(n2)
         dic[n2].append(n1)
-    stack =[0]
-    answer+=abs(a[0])
+    stack =[[0,0]]
     while stack:
-        node = stack.pop()
-        next_nodes = dic[node]
-        for next_node in next_nodes:
-            dic[next_node].remove(node)
-            stack.append(next_node)
-            answer+=abs(a[next_node])
-            a[node]+=a[next_node]
-            a[next_node]=0
+        print(stack)
+        node, prev_node = stack.pop()
+        if not dic[node]:
+            answer +=abs(a[node])
+            a[prev_node]+=a[node]
+            a[node]=0
+        else :
+            next_nodes = dic[node]
+            for next_node in next_nodes:
+                dic[next_node].remove(node)
+                stack.append([next_node,node])
     if sum(a)==0:
-        return answer-1
+        return answer+abs(a[0])
     else :
         return -1
 '''
-answer = 0
+
 def my(a, edges):
+    answer = []
     if sum(a) != 0:
         return -1
     graph = collections.defaultdict(list)
@@ -38,15 +40,14 @@ def my(a, edges):
         graph[u].append(v)
         graph[v].append(u)
     def dfs(child, parent):
-        global answer
         for node in graph[child]:
             if node != parent:
                 dfs(node,child)
-        answer += abs(a[child])
+        answer.append(abs(a[child]))
         a[parent] += a[child]
         a[child] = 0
     dfs(0,0)
-    return answer
+    return sum(answer)
 
 TC = int(input())
 for test_case in range(1, TC+1):
