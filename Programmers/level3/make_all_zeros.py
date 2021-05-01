633d6f505b7e7079e149ab169b34baa18f241ac3
SangMin-Code <ragdexplorer@gmail.com>
2021-05-01 17:30:59 +0900

    2021.4.30 make_all_zeros:reculsiv backtracking, iterative

--- Programmers/level3/make_all_zeros.py
+++ Programmers/level3/make_all_zeros.py
@@ -3,7 +3,9 @@ import sys
 from typing import List
 import collections
 sys.stdin = open('input/make_all_zeros')
+
 '''
+#시간초과 걸림
 def my(a:List[int], edges:[List[List[int]]])->int:
     answer = 0
     if sum(a)!=0:
@@ -12,25 +14,39 @@ def my(a:List[int], edges:[List[List[int]]])->int:
     for n1,n2 in edges:
         dic[n1].append(n2)
         dic[n2].append(n1)
-    stack =[[0,0]]
-    while stack:
-        print(stack)
-        node, prev_node = stack.pop()
-        if not dic[node]:
-            answer +=abs(a[node])
-            a[prev_node]+=a[node]
+    queue = collections.deque()
+    for i in dic:
+        if len(dic[i])==1:
+            queue.append(i)
+    while queue:
+        node = queue.popleft()
+        for next_node in dic[node]:
+            answer+=abs(a[node])
+            a[next_node]+=a[node]
             a[node]=0
-        else :
-            next_nodes = dic[node]
-            for next_node in next_nodes:
-                dic[next_node].remove(node)
-                stack.append([next_node,node])
-    if sum(a)==0:
-        return answer+abs(a[0])
-    else :
-        return -1
+            dic[next_node].remove(node)
+            if len(dic[next_node])==1:
+                queue.append(next_node)
+    return answer
 '''
-
+def my(a:List[int], edges:[List[List[int]]])->int:
+    if sum(a) != 0:
+        return -1
+    graph = collections.defaultdict(list)
+    for u,v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+    def dfs(c,p):
+        cnt = 0
+        for next_node in graph[c]:
+            if next_node != p:
+                cnt+=dfs(next_node,c)
+        cnt += abs(a[c])
+        a[p]+=a[c]
+        a[c]=0
+        return cnt
+    return dfs(0,0)
+'''
 def my(a, edges):
     answer = []
     if sum(a) != 0:
@@ -48,7 +64,7 @@ def my(a, edges):
         a[child] = 0
     dfs(0,0)
     return sum(answer)
-
+'''
 TC = int(input())
 for test_case in range(1, TC+1):
     a = list(map(int,input().split()))
