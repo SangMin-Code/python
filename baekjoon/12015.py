9f2669425bac0e8479fd275d909cff538391375f
SangMin-Code <ragdexplorer@gmail.com>
2021-06-01 15:43:04 +0900

    2021.6.1 이진검색 스터디

--- baekjoon/12015.py
+++ baekjoon/12015.py
@@ -1,7 +1,7 @@
 # 12015.py
 import sys
 from typing import List
-
+import bisect
 sys.stdin = open('input/12015')
 
 
@@ -32,9 +32,20 @@ def my(n:int, nums:List[int])->int:
         cnt += 1
     return cnt
 
+def my2(n:int,nums:List[int])->int:
+    dp = []
+
+    for i in nums:
+        k = bisect.bisect_left(dp, i)  # 자신이 들어갈 위치 k
+        if len(dp) <= k:  # i가 가장 큰 숫자라면
+            dp.append(i)
+        else:
+            dp[k] = i  # 자신보다 큰 수 중 최솟값과 대체
+    return len(dp)
+
 TC = int(input())
 for test_case in range(1, TC + 1):
     n = int(sys.stdin.readline().rstrip())
     nums = list(map(int,sys.stdin.readline().rstrip().split()))
-    answer = my(n,nums)
+    answer = my2(n,nums)
     print(answer)
