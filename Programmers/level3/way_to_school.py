f325dc1c22883aea34d01150cda9a0f5da2ac640
SangMin-Code <ragdexplorer@gmail.com>
2021-06-27 10:32:57 +0900

    2021.6.27 스터디

--- Programmers/level3/way_to_school.py
+++ Programmers/level3/way_to_school.py
@@ -18,9 +18,26 @@ def my(m:int,n:int,puddles:List[List[int]])->int:
                     ways[i][j] += ways[i - 1][j]
     return ways[n][m] % 1000000007   #최단경로의 개수를 1,000,000,007로 나눈 나머지를 return
 
+def my2(m:int,n:int,puddles:List[List[int]])->int:
+    ways= [[0]*(m+1) for _ in range(n+1)]
+    ways[1][1]=1
+
+    for r in range(1,n+1):
+        for c in range(1,m+1):
+            if [c,r] in puddles:
+                continue
+            else:
+                if ways[r][c-1] not in puddles:
+                    ways[r][c]+=ways[r][c-1]
+                if ways[r-1][c] not in puddles:
+                    ways[r][c] +=ways[r-1][c]
+    return ways[n][m] % 1000000007
+
+
+
 TC = int(input())
 for test_case in range(1,TC+1):
     m,n = map(int,input().split())
     puddles = [list(map(int,i.split(','))) for i in list(input().split())]
-    answer = my(m,n,puddles)
+    answer = my2(m,n,puddles)
     print(answer)
