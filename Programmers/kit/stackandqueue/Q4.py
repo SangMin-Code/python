6e5dc73c1cd876685c4b7d875e12e85eccb029e3
SangMin-Code <ragdexplorer@gmail.com>
2021-07-17 16:42:11 +0900

    2021.7.16 프로그래머스 고득점 우선순위큐

--- Programmers/kit/stackandqueue/Q4.py
+++ Programmers/kit/stackandqueue/Q4.py
@@ -17,6 +17,17 @@ def my(prices:List[int])->List[int]:
         stack.append([i,j])
     return answer
 
+def my2(prices:List[int])->List[int]:
+    prices = [[i,j] for i,j in enumerate(prices)]
+    stack =[]
+    answer = [len(prices)-x-1 for x in range(len(prices))]
+
+    for i,j in prices:
+        while stack and j<stack[-1][1]:
+            p = stack.pop()
+            answer[p[0]]=i-p[0]
+        stack.append([i,j])
+    return answer
 
 TC = int(input())
 for test_case in range(1, TC + 1):
