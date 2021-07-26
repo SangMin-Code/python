027030b0c9c35362b4a97c41cbdae76a7c022fba
SangMin-Code <ragdexplorer@gmail.com>
2021-07-26 16:32:29 +0900

    2021.7.26 프로그래머스 고득점 DFS_BFS

---
+++ Programmers/kit/DFS_BFS/Q1.py
@@ -0,0 +1,25 @@
+# Q1.py
+import sys
+from typing import List
+
+sys.stdin = open('input/Q1')
+
+
+def my(numbers:List[int],target:int)->int:
+    def dfs(sum,k):
+        if k == len(numbers):
+            if sum ==target:
+                return 1
+            else :
+                return 0
+        else :
+            return dfs(sum+numbers[k],k+1)+dfs(sum-numbers[k],k+1)
+    return dfs(0,0)
+
+
+TC = int(input())
+for test_case in range(1, TC + 1):
+    numbers = list(map(int,input().split()))
+    target = int(input())
+    answer = my(numbers,target)
+    print(answer)
