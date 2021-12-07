7196e6ed98be5377371b643c94ad5abe8ba89408
SangMin-Code <ragdexplorer@gmail.com>
2021-12-07 17:15:42 +0900

    2021.12.07 practice

---
+++ Programmers/level1/lottos.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('Programmers/level1/input/lottos.text')
+
+
+def solution(lottos, win_nums):
+    answer = [lottos.count(0), 0]
+    score = [6, 6, 5, 4, 3, 2, 1]
+    for i in range(6):
+        if lottos[i] in win_nums:
+            answer[0] += 1
+            answer[1] += 1
+    answer = [score[answer[0]], score[answer[1]]]
+    return answer
+
+
+TC = int(input())
+for i in range(TC):
+    lottos = list(map(int, input().split()))
+    win_nums = list(map(int, input().split()))
+    print(solution(lottos, win_nums))
