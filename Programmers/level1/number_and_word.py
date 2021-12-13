b02e0e69a89a3691190e3965ec57ed9a8370172b
SangMin-Code <ragdexplorer@gmail.com>
2021-12-13 10:37:56 +0900

    2021.12.13 practice

---
+++ Programmers/level1/number_and_word.py
@@ -0,0 +1,34 @@
+import sys
+from typing import List
+
+sys.stdin = open('Programmers/level1/input/number_and_word.text')
+
+
+def solution(s: str) -> str:
+
+    if s.isdecimal():
+        return int(s)
+
+    table = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+    }
+
+    for key, value in table.items():
+        if key in s:
+            s = s.replace(key, value)
+    return int(s)
+
+
+TC = int(input())
+for i in range(TC):
+    result = solution(input())
+    print(result)
