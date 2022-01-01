a4989070e11216d1a9341157a0aa8d223e6efc9f
SangMin-Code <ragdexplorer@gmail.com>
2022-01-01 18:27:06 +0900

    2021.1.1 practice

---
+++ Programmers/level2/cutting_n_squre_array.py
@@ -0,0 +1,23 @@
+import sys
+from typing import List
+sys.stdin = open('Programmers/level2/input/cutting_n_squre_array.txt')
+
+
+def solution(n: int, left: int, right: int) -> List[int]:
+
+    matrix = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(i+1):
+            matrix[i][j] = i+1
+            matrix[j][i] = i+1
+    array = []
+    for i in matrix:
+        array += i
+    return array[left:right+1]
+
+
+TC = int(input())
+for i in range(TC):
+    n, left, right = map(int, input().split())
+    result = solution(n, left, right)
+    print(result)
