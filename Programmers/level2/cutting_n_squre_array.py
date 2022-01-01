1d239681fdb05cec5ddd8eadc5083484f5ff28a6
SangMin-Code <ragdexplorer@gmail.com>
2022-01-01 18:44:29 +0900

    2021.1.1 practice

--- Programmers/level2/cutting_n_squre_array.py
+++ Programmers/level2/cutting_n_squre_array.py
@@ -5,15 +5,19 @@ sys.stdin = open('Programmers/level2/input/cutting_n_squre_array.txt')
 
 def solution(n: int, left: int, right: int) -> List[int]:
 
-    matrix = [[0]*n for _ in range(n)]
-    for i in range(n):
-        for j in range(i+1):
-            matrix[i][j] = i+1
-            matrix[j][i] = i+1
-    array = []
-    for i in matrix:
-        array += i
-    return array[left:right+1]
+    # matrix = [[0]*n for _ in range(n)]
+    # for i in range(n):
+    #     for j in range(i+1):
+    #         matrix[i][j] = i+1
+    #         matrix[j][i] = i+1
+    # array = []
+    # for i in matrix:
+    #     array += i
+    # return array[left:right+1]
+    answer = []
+    for i in range(left, right+1):
+        answer.append(max(i % n, i//n)+1)
+    return answer
 
 
 TC = int(input())
